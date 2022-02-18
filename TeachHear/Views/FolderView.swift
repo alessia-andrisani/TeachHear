db70ac7eca809775298a5f3f81ef3462ce1a64dc
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-02-18 12:38:23 +0100

    Used ExerciseItem in FolderView

--- TeachHear/Views/FolderView.swift
+++ TeachHear/Views/FolderView.swift
@@ -8,45 +8,18 @@
 import SwiftUI
 
 struct FolderView: View {
-	
 	private let columns: [GridItem] =  [GridItem(.adaptive(minimum: 400))]
 	
     var body: some View {
 		ScrollView(.vertical, showsIndicators: true) {
 			LazyVGrid(columns: columns, spacing: 50) {
 				ForEach(0..<14) { _ in
-					
-					NavigationLink(destination: ExerciseView()) {
-						HStack {
-							ZStack(alignment: .leading) {
-								RoundedRectangle(cornerRadius: 13)
-									.strokeBorder(lineWidth: 0.1)
-									.foregroundColor(.secondary)
-									.frame(width: 360, height: 160)
-									.background(Color(uiColor: .systemGray6))
-									.mask(RoundedRectangle(cornerRadius: 7))
-									.shadow(color: Color(uiColor: .systemGray5), radius: 5, x: 2, y: 5)
-								HStack {
-									RoundedRectangle(cornerRadius: 10)
-										.foregroundColor(.indigo)
-										.frame(width: 120, height: 120)
-										.padding()
-									VStack {
-										Text("Save Your Tears")
-											.fontWeight(.semibold)
-											.font(.system(size: 20))
-										Text("Sentence Scramble")
-											.font(.system(size: 17))
-									}
-								}
-							}
-						}
-						.padding(.trailing)
-					}
+					ExerciseItem()
 				}
 			}
 		}
 		.navigationTitle("Simple Present")
+		.background(Color(uiColor: .systemGroupedBackground))
     }
 }
 
