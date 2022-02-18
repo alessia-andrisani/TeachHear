49f0d6e9e67d1cfad12f16be4a97de92310d9e0a
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-02-18 10:59:41 +0100

    Added FolderItem

--- TeachHear/Views/ContentViewComponents/FoldersSection.swift
+++ TeachHear/Views/ContentViewComponents/FoldersSection.swift
@@ -10,30 +10,17 @@ import SwiftUI
 struct FoldersSection: View {
 	var body: some View {
 		VStack(alignment: .leading) {
-			Text("Folders")
-				.fontWeight(.bold)
-				.font(.system(size: 25))
+			Header("Folders")
+			
 			ScrollView(.horizontal, showsIndicators: false) {
-				HStack(spacing: 50) {
+				HStack(spacing: 20) {
 					ForEach(0..<10) { _ in
-						
-						NavigationLink(destination: FolderView()) {
-							ZStack {
-								Image("Folder")
-									.shadow(color: Color(uiColor: .systemGray5), radius: 5, x: 3, y: 5)
-								Text("Simple Present")
-									.font(.system(size: 20))
-									.fontWeight(.bold)
-									.foregroundColor(.indigo)
-							}
-						}
+						FolderItem()
 					}
 				}
-				.padding(.trailing)
+				.padding(.horizontal, 20)
 			}
-			
 		}
-		.padding(.all)
 	}
 }
 
