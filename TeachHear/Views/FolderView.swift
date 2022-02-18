1a46420db73b3916307ca6c88384fca5bb2bb4f6
alessia-andrisani <alessiaandrisani@virgilio.it>
2022-02-18 16:14:08 +0100

    Changed background color and put Folder Items in folder view.

--- TeachHear/Views/FolderView.swift
+++ TeachHear/Views/FolderView.swift
@@ -17,36 +17,14 @@ struct FolderView: View {
 				ForEach(0..<14) { _ in
 					
 					NavigationLink(destination: ExerciseView()) {
-						HStack {
-							ZStack(alignment: .leading) {
-								RoundedRectangle(cornerRadius: 13)
-									.strokeBorder(lineWidth: 0.1)
-									.foregroundColor(.secondary)
-									.frame(width: 360, height: 160)
-									.background(Color(uiColor: .systemGray6))
-									.mask(RoundedRectangle(cornerRadius: 7))
-									.shadow(color: Color(uiColor: .systemGray5), radius: 5, x: 2, y: 5)
-								HStack {
-									RoundedRectangle(cornerRadius: 10)
-										.foregroundColor(.indigo)
-										.frame(width: 120, height: 120)
-										.padding()
-									VStack {
-										Text("Save Your Tears")
-											.fontWeight(.semibold)
-											.font(.system(size: 20))
-										Text("Sentence Scramble")
-											.font(.system(size: 17))
-									}
-								}
-							}
-						}
-						.padding(.trailing)
+						ExerciseItem()
 					}
 				}
 			}
 		}
 		.navigationTitle("Simple Present")
+		.background(Color(uiColor: .systemGroupedBackground))
+		
     }
 }
 
