a2d24469393ce18fec2db339b982c24d1a48680f
Antonio Scognamiglio <tonyscogna95@gmail.com>
2022-02-24 15:21:23 +0100

    Added the youtube player

---
+++ TeachHear/Views/YouTubePlayer.swift
@@ -0,0 +1,38 @@
+//
+//  YouTubePlayer.swift
+//  YouTubePlayerSwiftUI
+//
+//  Created by Antonio Scognamiglio on 21/02/22.
+//
+
+import SwiftUI
+import UIKit
+import YouTubeiOSPlayerHelper
+
+struct YouTubePlayer: UIViewRepresentable {
+	let trackID: String
+	
+	class Coordinator: NSObject, YTPlayerViewDelegate {
+		func playerViewDidBecomeReady(_ playerView: YTPlayerView) { }
+	}
+	
+	func makeCoordinator() -> Coordinator {
+		return Coordinator()
+	}
+	
+	func makeUIView(context: Context) -> YTPlayerView {
+		let ytPlayerView = YTPlayerView()
+		ytPlayerView.delegate = context.coordinator
+		ytPlayerView.load(withVideoId: trackID)
+		
+		return ytPlayerView
+	}
+	
+	func updateUIView(_ uiView: YTPlayerView, context: Context) { }
+}
+
+struct YouTubePlayer_Previews: PreviewProvider {
+	static var previews: some View {
+		YouTubePlayer(trackID: "XXYlFuWEuKI")
+	}
+}
