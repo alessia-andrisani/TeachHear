844ed880148929e4e70f67bb3c486dbee04fdade
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-02-28 11:19:33 +0100

    Fixed infinite loop for ellipsis in lyrics

--- TeachHear/Extensions/String.swift
+++ TeachHear/Extensions/String.swift
@@ -8,45 +8,49 @@
 import Foundation
 
 extension String {
-    static let exampleLyrics = """
-    Una mattina mi sono alzato
-    O bella ciao, bella ciao, bella ciao, ciao, ciao
-    Una mattina mi sono alzato
-    E ho trovato l'invasor
-    
-    O partigiano, portami via
-    O bella ciao, bella ciao, bella ciao, ciao, ciao
-    O partigiano, portami via
-    Ché mi sento di morir
-    
-    E se io muoio da partigiano
-    O bella ciao, bella ciao, bella ciao, ciao, ciao
-    E se io muoio da partigiano
-    Tu mi devi seppellir
-    
-    E seppellire lassù in montagna
-    O bella ciao, bella ciao, bella ciao, ciao, ciao
-    E seppellire lassù in montagna
-    Sotto l'ombra di un bel fior
-    
-    Tutte le genti che passeranno
-    O bella ciao, bella ciao, bella ciao ciao ciao
-    E le genti che passeranno
-    Mi diranno: "Che bel fior"
-    
-    E questo è il fiore del partigiano
-    O bella ciao, bella ciao, bella ciao ciao ciao
-    Questo è il fiore del partigiano
-    Morto per la libertà
-    
-    E questo è il fiore del partigiano
-    Morto per la libertà
-    """
-    
+	static let exampleLyrics = """
+	Una mattina mi sono alzato
+	O bella ciao, bella ciao, bella ciao, ciao, ciao
+	Una mattina mi sono alzato
+	E ho trovato l'invasor
+	
+	O partigiano, portami via
+	O bella ciao, bella ciao, bella ciao, ciao, ciao
+	O partigiano, portami via
+	Ché mi sento di morir
+	
+	E se io muoio da partigiano
+	O bella ciao, bella ciao, bella ciao, ciao, ciao
+	E se io muoio da partigiano
+	Tu mi devi seppellir
+	
+	E seppellire lassù in montagna
+	O bella ciao, bella ciao, bella ciao, ciao, ciao
+	E seppellire lassù in montagna
+	Sotto l'ombra di un bel fior
+	
+	Tutte le genti che passeranno
+	O bella ciao, bella ciao, bella ciao ciao ciao
+	E le genti che passeranno
+	Mi diranno: "Che bel fior"
+	
+	E questo è il fiore del partigiano
+	O bella ciao, bella ciao, bella ciao ciao ciao
+	Questo è il fiore del partigiano
+	Morto per la libertà
+	
+	E questo è il fiore del partigiano
+	Morto per la libertà
+	"""
+	
+	fileprivate func containsMultipleCharacters() -> Bool {
+		!self.dropFirst().allSatisfy { $0 == self.first }
+	}
+	
 	func wordScramble() -> String {
 		let shuffled = String(shuffled())
 		
-		if count != 1 && shuffled == self {
+		if count != 1 && shuffled == self && containsMultipleCharacters() {
 			return wordScramble()
 		} else {
 			return shuffled.lowercased()
