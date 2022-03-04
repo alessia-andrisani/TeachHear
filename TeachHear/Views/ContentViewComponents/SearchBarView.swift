d5b4bf381a6bd6d8bde1c62f22b28aac1ece9f86
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-03-04 15:53:51 +0100

    Improved SearchBarView layout and logic

--- TeachHear/Views/ContentViewComponents/SearchBarView.swift
+++ TeachHear/Views/ContentViewComponents/SearchBarView.swift
@@ -8,68 +8,56 @@
 import SwiftUI
 
 struct SearchBarView: View {
-    @EnvironmentObject var IDTTrackManager: IDTrackManager
-    @StateObject var APIMManager = APIManager()
-    
-    @State var userInput : String = ""
-    @State var searchButton : Bool = true
-    
-   
-    
-    var body: some View {
-        HStack {
-            Image(systemName: "magnifyingglass")
-                .foregroundColor(Color(uiColor: .systemGray2))
-            
-            TextField("Type artist and song name...", text: $userInput)
-                .onSubmit {
-                    searchButton.toggle()
-                }
-                .disableAutocorrection(true)
-            
-            if !userInput.isEmpty {
-            Button {
-                userInput = ""
-            } label: {
-                Image(systemName: "xmark.circle.fill")
-                    .foregroundColor(Color(uiColor: .systemGray2))
-            }
-            .padding(.trailing, 8)
-            }
-            
-        }
-        .padding()
-        .background(Color(uiColor: .secondarySystemGroupedBackground))
-        .cornerRadius(.infinity)
-        .padding()
-        .onChange(of: searchButton, perform:  { value in
-            let pronnedInput = userInput.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
-            
-            if userInput == "" {
-                
-            } else {
-            
-                Task {
-                    await APIMManager.fetchData(userInput: pronnedInput!)
-            }
-            }
-        })
-        .onChange(of: userInput, perform:  { value in
-            if userInput == "" {
-                IDTTrackManager.listAppear = false
-            }
-        })
-        
-    }
+	@EnvironmentObject var idTrackManager: IDTrackManager
+	
+	@StateObject var apiManager = APIManager()
+	
+	@State var userInput : String = ""
+	@State var searchButton : Bool = true
+	
+	var body: some View {
+		HStack {
+			Image(systemName: "magnifyingglass")
+				.foregroundColor(Color(uiColor: .systemGray2))
+			
+			TextField("Type artist and song name...", text: $userInput)
+				.disableAutocorrection(true)
+				.onSubmit {
+					guard !userInput.isEmpty else { return }
+					
+					let pronnedInput = userInput.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+					
+					Task {
+						await apiManager.fetchData(userInput: pronnedInput!)
+					}
+				}
+			
+			if !userInput.isEmpty {
+				Button {
+					userInput = ""
+				} label: {
+					Image(systemName: "xmark.circle.fill")
+						.foregroundColor(Color(uiColor: .systemGray2))
+				}
+				.padding(.trailing, 8)
+			}
+			
+		}
+		.padding()
+		.background(Color(uiColor: .secondarySystemGroupedBackground))
+		.cornerRadius(.infinity)
+		.padding(.horizontal, 20)
+		.onChange(of: userInput) { newValue in
+			if newValue.isEmpty {
+				idTrackManager.listAppear = false
+			}
+		}
+	}
 }
-
-
 
 struct SearchBarView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchBarView()
-            .previewInterfaceOrientation(.landscapeRight)
-    }
+	static var previews: some View {
+		SearchBarView()
+			.previewInterfaceOrientation(.landscapeRight)
+	}
 }
-
-
