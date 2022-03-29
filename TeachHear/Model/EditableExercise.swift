696f6b1e712a9757068eb1e524093b06cc01f6f5
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-03-29 11:40:49 +0200

    Simplified EditableExercise initialiser

--- TeachHear/Model/EditableExercise.swift
+++ TeachHear/Model/EditableExercise.swift
@@ -22,9 +22,7 @@ class EditableExercise: ObservableObject {
 				  date: .now,
 				  title: cleanTitle,
 				  type: .wordScramble,
-				  lyrics: song.lyrics,
-				  originalLyrics: song.lyrics,
-				  isNew: true)
+				  originalLyrics: song.lyrics)
 	}
 	
 	convenience init(_ exercise: CoreExercise) {
@@ -33,22 +31,20 @@ class EditableExercise: ObservableObject {
 				  title: exercise.wrappedTitle,
 				  type: ExerciseType(rawValue: exercise.type ?? "Word Scramble") ?? .wordScramble,
 				  lyrics: exercise.wrappedLyrics,
-				  originalLyrics: exercise.song?.wrappedLyrics ?? "",
-				  isNew: false)
+				  originalLyrics: exercise.song?.wrappedLyrics ?? "")
 	}
 	
 	init(id: String,
 		 date: Date,
 		 title: String,
 		 type: ExerciseType,
-		 lyrics: String,
-		 originalLyrics: String,
-		 isNew: Bool) {
+		 lyrics: String? = nil,
+		 originalLyrics: String) {
 		self.id = id
 		self.date = date
 		self.title = title
 		self.type = type
-		self.isNew = isNew
+		self.isNew = lyrics == nil
 		
 		func splitIntoArrays(_ text: String) -> [[String]] {
 			let textWithSections = text.replacingOccurrences(of: "\n\n", with: "\n\u{200b}\n")  // Inserts a invisible character to keep the sections of the text
@@ -56,27 +52,24 @@ class EditableExercise: ObservableObject {
 			return lines.map { $0.split(separator: " ").map { String($0) } }
 		}
 		
-		self.words = splitIntoArrays(lyrics)
+		self.words = splitIntoArrays(lyrics ?? originalLyrics)
 		self.originalLyrics = originalLyrics
 		self.originalWords = splitIntoArrays(originalLyrics)
 	}
 	
+	let id: String
+	let date: Date
+	let isNew: Bool
 	let originalLyrics: String
 	let originalWords: [[String]]
 	
-	let id: String
-	@Published var date: Date
 	@Published var title: String
 	@Published var type: ExerciseType
-//	@Published var song: CoreSong
 	@Published var words: [[String]]
-	let isNew: Bool
 	
 	static var exampleExercise = EditableExercise(id: UUID().uuidString,
 												  date: .now,
 												  title: "Bella Ciao",
 												  type: .wordScramble,
-												  lyrics: .exampleLyrics,
-												  originalLyrics: .exampleLyrics,
-												  isNew: true)
+												  originalLyrics: .exampleLyrics)
 }
